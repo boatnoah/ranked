af765cb91f4e453f78f36d028f6b60e689408e43
Noah <noahskim7@gmail.com>
2026-02-24 14:33:57 -0800

    Add abstraction methods for redis sorted sets

--- internal/sortedsets/redis.go
+++ internal/sortedsets/redis.go
@@ -1,6 +1,11 @@
 package sortedsets
 
-import "github.com/go-redis/redis/v8"
+import (
+	"context"
+	"errors"
+
+	"github.com/go-redis/redis/v8"
+)
 
 func NewRedisClient(addr, pw string, db int) *redis.Client {
 	return redis.NewClient(&redis.Options{
@@ -8,4 +13,44 @@ func NewRedisClient(addr, pw string, db int) *redis.Client {
 		Password: pw,
 		DB:       db,
 	})
+}
+
+// functions
+// Add Score
+// Retrieve Top Player
+
+func AddPlayer(ctx context.Context, client *redis.Client, score float64, email string) error {
+	if email == "" {
+		return errors.New("Invalid email")
+	}
+
+	member := redis.Z{
+		Score:  score,
+		Member: email,
+	}
+
+	client.ZAdd(ctx, "leaderboard", &member)
+
+	return nil
+}
+
+func GetUserRank(ctx context.Context, client *redis.Client, email string) (int64, error) {
+	rank, err := client.ZRevRank(ctx, "leaderboard", email).Result()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return rank, nil
+}
+
+func GetTopPlayers(ctx context.Context, client *redis.Client) ([]redis.Z, error) {
+	leaderboard, err := client.ZRevRangeWithScores(ctx, "leaderboard", 0, 2).Result()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return leaderboard, nil
+
 }
